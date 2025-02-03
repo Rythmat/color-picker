126f43187f23c516487d7b2646921447deb7dbe4
Rythmat <teotaniwha@gmail.com>
2025-02-03 14:56:34 -0700

    Color Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import ColorButton from './ColorButton.jsx'
+import PickedColor from './PickedColor'
+
 const App = () => {
   return (
     <>
-      <h1>TEST</h1>
+      <h1>Color Picker</h1>
+      <ColorButton color="Blue"/>
+      
+      <ColorButton color="Yellow"/>
+
+      <ColorButton color="Green"/>
     </>
   )
 }
---
+++ src/ColorButton.jsx
@@ -0,0 +1,6 @@
+const ColorButton = (props) => {
+  const color = props.color
+  return <button>{color}</button>
+}
+
+export default ColorButton;
